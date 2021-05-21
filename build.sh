90e171f14bb0981eb43ee7f54ae7510b91f66db2
BARMCO <me@barm.co>
2021-05-22 01:43:49 +0900

    fix: modify build output to regularized name

--- build.sh
+++ build.sh
@@ -18,4 +18,10 @@ else
   rm -rf "$PREV/.build"
 fi
 cp -r ./build/ "$PREV/.build"
+
+ARTIFACT_NAME=$(sed -n 's/^rootProject.name = '"'"'\(.*\)'"'"'$/\1/p' ./settings.gradle)
+VERSION_NAME=$(sed -n 's/^version '"'"'\(.*\)'"'"'$/\1/p' ./build.gradle)
+JAR_ARCHIVE_NAME="$ARTIFACT_NAME"-"${VERSION_NAME}".jar
+
+mv "$PREV/.build/libs/$JAR_ARCHIVE_NAME" "$PREV/.build/out.jar"
 cd "$PREV" || return
